e6aeb61f3395c83d469dfc5384c5ced46fb44fa0
felicianapripon@gmail.com <felicianapripon@gmail.com>
2015-09-25 17:29:29 +0300

    test

---
+++ src/main/java/com/java/Main.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class Main {
+	public static void main(String[] args){
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("text 1");
+		System.out.println("text 2");
+	}	
+}
